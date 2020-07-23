55744e2c77aa1417855dad99c0255fb1b28767a3
Gert <winone922@gmail.com>
2020-07-23 15:10:16 +0800

    Update PhoneDx.java

--- src/main/java/top/fotg/util/PhoneDx.java
+++ src/main/java/top/fotg/util/PhoneDx.java
@@ -41,11 +41,11 @@ public class PhoneDx {
         System.setProperty("sun.net.client.defaultConnectTimeout", "10000");
         System.setProperty("sun.net.client.defaultReadTimeout", "10000");
         // 初始化ascClient需要的几个参数
-        final String product = "Dysmsapi";// 短信API产品名称（短信产品名固定，无需修改）
-        final String domain = "dysmsapi.aliyuncs.com";// 短信API产品域名（接口地址固定，无需修改）
+        final String product = "";// 短信API产品名称（短信产品名固定，无需修改）
+        final String domain = "";// 短信API产品域名（接口地址固定，无需修改）
         // 替换成你的AK
-        final String accessKeyId = "LTAI67F9KGgsAhn6";// 你的accessKeyId,参考本文档步骤2
-        final String accessKeySecret = "JJVCoDK7YMkmAAyTqalLvMvwkwKOJI";// 你的accessKeySecret，参考本文档步骤2
+        final String accessKeyId = "";// 你的accessKeyId,参考本文档步骤2
+        final String accessKeySecret = "";// 你的accessKeySecret，参考本文档步骤2
         // 初始化ascClient,暂时不支持多region（请勿修改）
         IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
         DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
